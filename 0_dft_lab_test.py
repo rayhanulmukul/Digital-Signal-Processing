1d2bdbaf4c8de41f71446ee6049be8964a08c05c
Md Rayhanul Islam <73143090+rayhanulmukul@users.noreply.github.com>
2024-11-07 10:21:37 +0600

    inverse dft

--- 0_dft_lab_test.py
+++ 0_dft_lab_test.py
@@ -1,96 +1,131 @@
-# 1. Write a python program for generating a composite signal (you could use sine or cosine waves). The parameters including the signal frequencies of 40 Hz, 80 Hz, 160 Hz with the amplitudes of 10, 20, and 40 respectively, and the signal length should be limited to 512 in samples.
-# 2. Plot the generated signal.
-# 3. Do standard sampling by following the Nyquist rate.
-# 4. Perform under sampling and over sampling too. Use Subplot function to show the original, sampled, under sampled, and over sampled signal.
-# 5. Then perform N=512 point DFT, show the magnitude and phase spectrum.
-
 import matplotlib.pyplot as plt
 import numpy as np
 import cmath as cm
 import math
 
-N = 512
-def DFT(x):
-    X = np.zeros(N, dtype=complex)
-    for k in range(N):
-        for n in range(N):
-            X[k] += x[n] * np.exp(-2j * np.pi * n * k / N)
-    return X
-
-def phase(X_n):
-    x_phase = []
-    for i in X_n:
-        phase = cm.phase(round(i.real) + round(i.imag) * 1j)
-        x_phase.append(math.degrees(phase))
-    return x_phase
-
-t = np.arange(0, 1, 1/N)
+# Complex Signal
+t = np.arange(0, 0.1, 0.0001)
 x = 10 * np.sin(2 * np.pi * 40 * t) + 20 * np.sin(2 * np.pi * 80 * t) + 40 * np.sin(2 * np.pi * 160 * t)
-t = t[:13] # 512/40 = 13
-x = x[:13]
-plt.subplot(2, 2, 1)
+plt.subplot(3, 2, 1)
 plt.plot(t, x)
-plt.title("Composite Signal")
+plt.title("Complex Signal")
 plt.xlabel("Time(s)")
 plt.ylabel("Amplitude")
-plt.grid()
 
-N = 512
-fs = 320 # Nyquist Rate = 2 * Fmax
-t = np.arange(0, N/fs, 1/fs)
+# Sampled Signal
+fs = 512
+t = np.arange(0, 1, 1/fs)
 x = 10 * np.sin(2 * np.pi * 40 * t) + 20 * np.sin(2 * np.pi * 80 * t) + 40 * np.sin(2 * np.pi * 160 * t)
-t = t[:8] # 512/40 = 13
+plt.subplot(3, 2, 2)
+t = t[:13]
+x = x[:13]
+plt.plot(t, x)
+plt.title("Sampled Signal")
+plt.xlabel("Time(s)")
+plt.ylabel("Amplitude")
+
+# Nyquist Rate Signal
+N = 512
+Fs = 320
+t = np.arange(0, N/Fs, 1/Fs)
+x = 10 * np.sin(2 * np.pi * 40 * t) + 20 * np.sin(2 * np.pi * 80 * t) + 40 * np.sin(2 * np.pi * 160 * t)
+plt.subplot(3, 2, 3)
+t = t[:8]
 x = x[:8]
-plt.subplot(2, 2, 2)
 plt.plot(t, x)
-plt.title("Sampled Signal (Nquist Rate)")
-plt.xlabel("n")
+plt.title("Nyquist Rate Signal")
+plt.xlabel("Time(s)")
 plt.ylabel("Amplitude")
 
-
-# Over Sampling
+# Over Sampled Signal
 N = 512
-fs = 640 # Nyquist Rate = 2 * Fmax = * 2
-t = np.arange(0, N/fs, 1/fs)
+Fs = 640
+t = np.arange(0, N/Fs, 1/Fs)
 x = 10 * np.sin(2 * np.pi * 40 * t) + 20 * np.sin(2 * np.pi * 80 * t) + 40 * np.sin(2 * np.pi * 160 * t)
-t = t[:16] # 512/40 = 13
+plt.subplot(3, 2, 4)
+t = t[:16]
 x = x[:16]
-plt.subplot(2, 2, 3)
 plt.plot(t, x)
-plt.title("Over Signal")
+plt.title("Over Sampled Signal")
+plt.xlabel("Time(s)")
+plt.ylabel("Amplitude")
+
+# Under Sampled Signal
+N = 512
+Fs = 160
+t = np.arange(0, N/Fs, 1/Fs)
+x = 10 * np.sin(2 * np.pi * 40 * t) + 20 * np.sin(2 * np.pi * 80 * t) + 40 * np.sin(2 * np.pi * 160 * t)
+plt.subplot(3, 2, 5)
+t = t[:5]
+x = x[:5]
+plt.plot(t, x)
+plt.title("Under Sampled Signal")
+plt.xlabel("Time(s)")
+plt.ylabel("Amplitude")
+
+plt.tight_layout()
+plt.show()
+
+
+def dft(x, N):
+    y = np.zeros(N, dtype=complex)
+    for k in range(N):
+        for n in range(N):
+            y[k] += x[n] * np.exp(-2j * np.pi * n * k / N)
+    return y
+
+def phase(x_n):
+    y = []
+    for i in x_n:
+        phase1 = cm.phase(round(i.real) + round(i.imag) * 1j)
+        y.append(math.degrees(phase1))
+    return y
+
+def idft(x_n, N):
+    y = np.zeros(N, dtype=complex)
+    for k in range(N):
+        for n in range(N):
+            y[k] += x_n[n] * np.exp(2j * np.pi * k * n / N)
+        y[k] = y[k] / N
+    return y
+
+# Magnitude Spectrum
+fs = 512
+t = np.arange(0, 1, 1/fs)
+x = 10 * np.sin(2 * np.pi * 40 * t) + 20 * np.sin(2 * np.pi * 80 * t) + 40 * np.sin(2 * np.pi * 160 * t)
+
+x_n = dft(x, len(x))
+# plt.subplot(2, 1, 1)
+# t = t[:13]
+# x_n = x_t[:13]
+# plt.stem(t, x_n)
+# plt.title("DFT")
+# plt.xlabel("n")
+# plt.ylabel("Amplitude")
+
+plt.subplot(3, 1, 1)
+plt.stem(np.abs(x_n))
+plt.title("Magnitude Spectrum")
 plt.xlabel("n")
 plt.ylabel("Amplitude")
 
-# Under Sampling
-N = 512
-fs = 160 # Nyquist Rate = 2 * Fmax = * 2
-t = np.arange(0, N/fs, 1/fs)
-x = 10 * np.sin(2 * np.pi * 40 * t) + 20 * np.sin(2 * np.pi * 80 * t) + 40 * np.sin(2 * np.pi * 160 * t)
-t = t[:5] # 512/40 = 13
-x = x[:5]
-plt.subplot(2, 2, 4)
-plt.plot(t, x)
-plt.title("Over Signal")
+# Phase Spectrum
+x_phase = phase(x_n)
+plt.subplot(3, 1, 2)
+plt.stem(x_phase)
+plt.title("Phase Spectrum")
+plt.xlabel("Angle (in Degrees)")
+plt.ylabel("Amplitude")
+
+# Reconstructed Signal(IDFT)
+x_idft = idft(x_n, len(x_n))
+plt.subplot(3, 1, 3)
+plt.plot(x_idft.real)
+plt.stem(x_idft.real)
+plt.title("Reconstructed Signal(IDFT)")
 plt.xlabel("n")
 plt.ylabel("Amplitude")
 
 plt.tight_layout()
 plt.show()
 
-N = 512
-fs = 320 # Nyquist Rate = 2 * Fmax
-t = np.arange(0, N/fs, 1/N)
-x_n = 10 * np.sin(2 * np.pi * 40 * t) + 20 * np.sin(2 * np.pi * 80 * t) + 40 * np.sin(2 * np.pi * 160 * t)
-X_n = DFT(x_n)
-plt.subplot(2, 1, 1)
-plt.stem(np.abs(X_n))
-plt.title("Magnitude Spectrum")
-plt.xlabel("n")
-plt.ylabel("Amplitude")
-
-
-x_phase = phase(X_n)
-plt.subplot(2, 1, 2)
-plt.stem(x_phase, basefmt="")
-
-plt.show()
--- test.py
+++ test.py
@@ -1,20 +1,131 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import cmath as cm
+import math
 
-def corelation(x):
-    sz = len(x)
-    length = (sz * 2) - 1
-    x_n = [0] * length
-    for k in range(length):
-        x_n[k] = sum(x[n] * x[n + k] for n in range(sz - k))
-    return x_n
+# Complex Signal
+t = np.arange(0, 0.1, 0.0001)
+x = 10 * np.sin(2 * np.pi * 40 * t) + 20 * np.sin(2 * np.pi * 80 * t) + 40 * np.sin(2 * np.pi * 160 * t)
+plt.subplot(3, 2, 1)
+plt.plot(t, x)
+plt.title("Complex Signal")
+plt.xlabel("Time(s)")
+plt.ylabel("Amplitude")
 
-# Sequence
-x = [1, 2, 3, 4]
-x_n = corelation(x)
-plt.stem(range(len(x_n)), x_n, use_line_collection=" ")
-plt.title("Auto Correlation")
-plt.xlabel('n')
-plt.ylabel('Amplitude')
+# Sampled Signal
+fs = 512
+t = np.arange(0, 1, 1/fs)
+x = 10 * np.sin(2 * np.pi * 40 * t) + 20 * np.sin(2 * np.pi * 80 * t) + 40 * np.sin(2 * np.pi * 160 * t)
+plt.subplot(3, 2, 2)
+t = t[:13]
+x = x[:13]
+plt.plot(t, x)
+plt.title("Sampled Signal")
+plt.xlabel("Time(s)")
+plt.ylabel("Amplitude")
 
+# Nyquist Rate Signal
+N = 512
+Fs = 320
+t = np.arange(0, N/Fs, 1/Fs)
+x = 10 * np.sin(2 * np.pi * 40 * t) + 20 * np.sin(2 * np.pi * 80 * t) + 40 * np.sin(2 * np.pi * 160 * t)
+plt.subplot(3, 2, 3)
+t = t[:8]
+x = x[:8]
+plt.plot(t, x)
+plt.title("Nyquist Rate Signal")
+plt.xlabel("Time(s)")
+plt.ylabel("Amplitude")
+
+# Over Sampled Signal
+N = 512
+Fs = 640
+t = np.arange(0, N/Fs, 1/Fs)
+x = 10 * np.sin(2 * np.pi * 40 * t) + 20 * np.sin(2 * np.pi * 80 * t) + 40 * np.sin(2 * np.pi * 160 * t)
+plt.subplot(3, 2, 4)
+t = t[:16]
+x = x[:16]
+plt.plot(t, x)
+plt.title("Over Sampled Signal")
+plt.xlabel("Time(s)")
+plt.ylabel("Amplitude")
+
+# Under Sampled Signal
+N = 512
+Fs = 160
+t = np.arange(0, N/Fs, 1/Fs)
+x = 10 * np.sin(2 * np.pi * 40 * t) + 20 * np.sin(2 * np.pi * 80 * t) + 40 * np.sin(2 * np.pi * 160 * t)
+plt.subplot(3, 2, 5)
+t = t[:5]
+x = x[:5]
+plt.plot(t, x)
+plt.title("Under Sampled Signal")
+plt.xlabel("Time(s)")
+plt.ylabel("Amplitude")
+
+plt.tight_layout()
 plt.show()
+
+
+def dft(x, N):
+    y = np.zeros(N, dtype=complex)
+    for k in range(N):
+        for n in range(N):
+            y[k] += x[n] * np.exp(-2j * np.pi * n * k / N)
+    return y
+
+def phase(x_n):
+    y = []
+    for i in x_n:
+        phase1 = cm.phase(round(i.real) + round(i.imag) * 1j)
+        y.append(math.degrees(phase1))
+    return y
+
+def idft(x_n, N):
+    y = np.zeros(N, dtype=complex)
+    for k in range(N):
+        for n in range(N):
+            y[k] += x_n[n] * np.exp(2j * np.pi * k * n / N)
+        y[k] = y[k] / N
+    return y
+
+# Magnitude Spectrum
+fs = 512
+t = np.arange(0, 1, 1/fs)
+x = 10 * np.sin(2 * np.pi * 40 * t) + 20 * np.sin(2 * np.pi * 80 * t) + 40 * np.sin(2 * np.pi * 160 * t)
+
+x_n = dft(x, len(x))
+# plt.subplot(2, 1, 1)
+# t = t[:13]
+# x_n = x_t[:13]
+# plt.stem(t, x_n)
+# plt.title("DFT")
+# plt.xlabel("n")
+# plt.ylabel("Amplitude")
+
+plt.subplot(3, 1, 1)
+plt.stem(np.abs(x_n))
+plt.title("Magnitude Spectrum")
+plt.xlabel("n")
+plt.ylabel("Amplitude")
+
+# Phase Spectrum
+x_phase = phase(x_n)
+plt.subplot(3, 1, 2)
+plt.stem(x_phase)
+plt.title("Phase Spectrum")
+plt.xlabel("Angle (in Degrees)")
+plt.ylabel("Amplitude")
+
+# Reconstructed Signal(IDFT)
+x_idft = idft(x_n, len(x_n))
+plt.subplot(3, 1, 3)
+plt.plot(x_idft.real)
+plt.stem(x_idft.real)
+plt.title("Reconstructed Signal(IDFT)")
+plt.xlabel("n")
+plt.ylabel("Amplitude")
+
+plt.tight_layout()
+plt.show()
+
